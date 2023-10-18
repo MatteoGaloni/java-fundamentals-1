fbd065fb0206e212deb3f8d47c0e2ea07fb5b77a
MatteoGaloni <mtgaloni@gmail.com>
2023-10-18 17:41:44 +0200

    add Solution B in PasswordGenerator

--- src/org/learning/java/PasswordGenerator.java
+++ src/org/learning/java/PasswordGenerator.java
@@ -1,5 +1,6 @@
 package org.learning.java;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PasswordGenerator {
@@ -11,15 +12,30 @@ public class PasswordGenerator {
         String familyName = scan.nextLine();
         System.out.println("What's your favourite color?");
         String color = scan.nextLine();
-        System.out.println("What day were you born?");
-        int day = scan.nextInt();
-        System.out.println("What month were you born?");
-        int month = scan.nextInt();
-        System.out.println("What year were you born");
-        int year = scan.nextInt();
+//        soluzione A
+//        System.out.println("What day were you born?");
+//        int day = scan.nextInt();
+//        System.out.println("What month were you born?");
+//        int month = scan.nextInt();
+//        System.out.println("What year were you born");
+//        int year = Integer.parseInt(scan.nextLine());
+
+//        soluzione B
+
+        System.out.println("Enter your birthday DD/MM/YYYY");
+        String birthday = scan.nextLine();
+
+        String [] formatBirthday = birthday.split("/");
+        System.out.println(Arrays.toString(formatBirthday));
+        int formattedDay = Integer.parseInt(formatBirthday[0]);
+        int formattedMonth = Integer.parseInt(formatBirthday[1]);
+        int formattedYear = Integer.parseInt(formatBirthday[2]);
+
+        int birthdaySum = formattedDay + formattedMonth + formattedYear;
 
         String specialChar = "-";
-        int birthdaySum = day + month + year;
+//        soluzione A
+//        int birthdaySum = day + month + year;
 
         String password = name + specialChar + familyName + specialChar + color + specialChar + birthdaySum;
 
