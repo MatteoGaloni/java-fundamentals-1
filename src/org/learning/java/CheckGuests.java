36132acbdbc2fbdaaaa5a9cadc14c9b2c7b1deeb
MatteoGaloni <mtgaloni@gmail.com>
2023-10-18 14:56:59 +0200

    add CheckGuests

---
+++ src/org/learning/java/CheckGuests.java
@@ -0,0 +1,28 @@
+package org.learning.java;
+
+import java.util.Scanner;
+
+public class CheckGuests {
+    public static void main(String[] args) {
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What's you name? ");
+        String guestName = scan.nextLine();
+
+        boolean isAuthorized = false;
+        for (int i = 0; i < guests.length ; i++) {
+            if (guestName.equals(guests[i])){
+                isAuthorized = true;
+                System.out.println("trovato");
+            }
+        }
+            if (isAuthorized){
+                System.out.println("Please Welcome");
+            } else {
+                System.out.println("Sorry, you are not allowed to enter");
+            }
+
+            scan.close();
+    }
+}
